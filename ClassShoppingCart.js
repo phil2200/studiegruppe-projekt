cbed8492a3ff01cbcdf0b9f570f93c9d3e2a97d4
Emma Dalgaard <emla19ac@student.cbs.com>
2019-10-09 16:38:01 +0200

    Shopping cart

---
+++ ClassShoppingCart.js
@@ -0,0 +1,14 @@
+class ClassShoppingCart {
+    // constructor angiver properties
+    constructor(cart_item_id, cart_session_id, product_id, quantity, purchase_amount, color, age, shipping_info) {
+        this.cart_item_id = cart_item_id;
+        this.cart_session_id = cart_session_id;
+        this.product_id = product_id;
+        this.quantity = quantity;
+        this.purchase_amount = purchase_amount;
+        this.color = color;
+        this.age = age;
+        this.shipping_info = shipping_info;
+    }
+
+}
