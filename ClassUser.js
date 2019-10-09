65d3628425ff5c322bb35136634905d712699129
Emil Juel Jakobsen <Emja19ai@student.cbs.dk>
2019-10-09 16:34:11 +0200

    HTML first code

--- ClassUser.js
+++ ClassUser.js
@@ -1,2 +1,24 @@
+
+
+class userLogin {
+    constructor (username, password, address, phone, email, city, zip) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.city = city;
+        this.zip = zip;
+    }
+
+
+
+
+    }
+
+    new userLogin()
+
+
 var username = document.getElementById("username")
 var password = document.getElementById("password")
+
