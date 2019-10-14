545dad5204e57ce7d132a008cd430f062b5815c7
ahellow <ahellow@users.noreply.github.com>
2019-10-14 10:11:44 +0200

    Update ClassOrders.js

--- ClassOrders.js
+++ ClassOrders.js
@@ -1,7 +1,7 @@
 class orders {
 
 // constructor angiver properties
-<<<<<<< HEAD
+
     constructor(cart_item_id, cart_session_id, product_id, quantity, age,) {
         this.cart_item_id = cart_item_id;
         this.cart_session_id = cart_session_id;
@@ -10,9 +10,10 @@ class orders {
         this.purchase_amount = purchase_amount;
         this.age = age;
     }
-}// sut
-=======
-constructor(cart_item_id, cart_session_id, product_id, quantity, age,) {
+}
+// sut
+constructor(cart_item_id, cart_session_id, product_id, quantity, age,)
+{
     this.cart_item_id = cart_item_id;
     this.cart_session_id = cart_session_id;
     this.product_id = product_id;
@@ -20,4 +21,4 @@ constructor(cart_item_id, cart_session_id, product_id, quantity, age,) {
     this.purchase_amount = purchase_amount;
     this.age = age;
 }
->>>>>>> 96697679f57f39cd0afcd39dfee9ac2f20bae3d0
+
