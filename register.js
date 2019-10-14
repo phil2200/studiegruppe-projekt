846a318d8da4b2cf64c3727c63b2d7ab6cfd7dbf
ahellow <ahellow@users.noreply.github.com>
2019-10-14 13:02:42 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,32 +1,63 @@
 
+//submit for log-in and sign-up, also amount of attempts allowed.
+var submit = document.getElementById("submit");
+var signup = document.getElementById("signup");
+var attempt = 3;
+
+// assigning existing user for test purposes
+var existingUser = JSON.parse(localStorage.getItem("user"));
+var currentLogin = [];
+
+// when clicking submit, retrieve input from username and password box.
+submit.onclick = function() {
+    var usernameInput = document.getElementById("username").value ;
+    var passwordInput = document.getElementById("password").value ;
+
+}
+
+
 //Get the entered details from HTML sign up page
 
-var username = document.getElementById(elementid: username)
+let username = document.getElementById("username");
 
-var phone = document.getElementById(elementid: phone)
+let phone = document.getElementById("phone");
 
-var address = document.getElementById(elementid: address)
+let address = document.getElementById("address");
 
-var email = document.getElementById(elementid: email)
+let email = document.getElementById("email");
 
-var password = document.getElementById(elementid: password)
+let password = document.getElementById( "password");
 
 
 
 // store these entered values to be called for later
 
 function storeDetails() {
-    localStorage.setItem("username", username.value)
-    localStorage.setItem("userphone", phone.value)
-    localStorage.setItem("address", address.value)
-    localStorage.setItem("useremail", email.value)
-    localStorage.setItem("password", password.value)
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("userphone", phone.value);
+    localStorage.setItem("address", address.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
 
 }
 
+
+
 // Show stored values
 function showDetails() {
-    console.log("show stored info")
+    console.log("show stored info");
     console.log(localStorage)
+
+}
+
+// function to compare stored vs. entered values
+
+function checkLogin() {
+    //get stored values
+    let storedName = localStorage.getItem( "username");
+    let storedPassword = localStorage.getItem( "password");
+
+    let enteredName = document.getElementById( "enteredName");
+    let enteredPassword = document.getElementById("enteredPassword");
 
 }
