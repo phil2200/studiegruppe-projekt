1e0974537a879497dd71dc7e9c405ade5905b91e
phil2200 <phjo19ab@student.cbs.dk>
2019-10-14 09:10:27 +0200

    hej

--- ClassOrders.js
+++ ClassOrders.js
@@ -1,11 +1,12 @@
 class orders {
 
 // constructor angiver properties
-constructor(cart_item_id, cart_session_id, product_id, quantity, age,) {
-    this.cart_item_id = cart_item_id;
-    this.cart_session_id = cart_session_id;
-    this.product_id = product_id;
-    this.quantity = quantity;
-    this.purchase_amount = purchase_amount;
-    this.age = age;
+    constructor(cart_item_id, cart_session_id, product_id, quantity, age,) {
+        this.cart_item_id = cart_item_id;
+        this.cart_session_id = cart_session_id;
+        this.product_id = product_id;
+        this.quantity = quantity;
+        this.purchase_amount = purchase_amount;
+        this.age = age;
+    }
 }// sut
