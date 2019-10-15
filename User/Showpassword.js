f38a5eb0a7702cc4acca168282b5b6ef8307ead5
ahellow <ahellow@users.noreply.github.com>
2019-10-15 12:38:50 +0200

    checkbox

---
+++ User/Showpassword.js
@@ -0,0 +1,11 @@
+
+//function der skjuler kodeord eller viser det.
+
+function showPassword() {
+    var x = document.getElementById("password");
+    if ( x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
