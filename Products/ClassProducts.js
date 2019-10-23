196d56c560f6256cbf32ad7b7b9d706d718311ce
Emil Juel Jakobsen <Emja19ai@student.cbs.dk>
2019-10-23 12:52:41 +0200

    FAQ + products js

--- Products/ClassProducts.js
+++ Products/ClassProducts.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 class userProfile {
+=======
+class Product {
+>>>>>>> Stashed changes
     constructor(productId, productName, productDesc, productShortDesc, productPrice, colors, productType,
                 quantity, productImage, viewCount, dateAdded ) {
 
