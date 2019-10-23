bfaa8203302c0f3335f9ae04dc84dca2c8bd218f
Emil Juel Jakobsen <Emja19ai@student.cbs.dk>
2019-10-23 16:03:58 +0200

    Products

--- products.js
+++ products.js
@@ -2,7 +2,21 @@ var product1 = new Product(0, "Black Hoodie", "Hoodie is black",
     "black sweat", 700, "black", "Hoodie", 1,
     "blackhoodie.jpg", 0, "0");
 
-var products = [product1];
+var product2 = new Product(0, "White Hoodie", "Hoodie is white",
+    "White sweat", 699, "white", "Hoodie", 1,
+    "whitehoodie.jpg","0", "0");
+
+var product3 = new Product(0, "Black T-shirt", "T-shirt is black",
+    "Black T-shirt", 399, "black", "T-shirt", 1,
+    "blacktshirt.jpg","0", "0");
+
+var product4 = new Product(0, "White T-shirt", "T-shirt is white",
+    "White T-shirt", 399, "white", "T-shirt", 1,
+    "whitetshirt.jpg","0", "0");
+
+
+
+var products = [product1, product2, product3, product4];
 
 var productsContainer = document.getElementById('productsContainer');
 
@@ -15,4 +29,6 @@ for(var i=0; i<products.length; i++){
    product.innerHTML += '<div class="productPrice"> Price: ' + products[i]._productPrice + ' kr </div>';
    productsContainer.appendChild(product);
 }
+
+
 
