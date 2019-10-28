3832f0fdaabd7227c94b714e842b7041a6bdfa17
Emma Dalgaard <emla19ac@student.cbs.com>
2019-10-28 19:31:58 +0100

    opdatering af userlogin

--- SignIn.js
+++ SignIn.js
@@ -8,28 +8,31 @@ function login() {
     //check if entered details match stored details. redirect to home page
     for (let i = 0; i < existingUser.length; i++) {
         if (usernameInput == existingUser[i].username && passwordInput == existingUser[i].password) {
-            location.href = "loggedIn.html"
+            //location.href = "loggedIn.html";
 
             //push username to current local storage
-            currentLogIn.push({username: usernameInput});
+            //currentLogIn.push({username: usernameInput});
+            // udkommenteret fordi currentLogin ikke findes.
 
 
             //window.open('/loggedln.html');
-            var IDString = JSON.stringify(currentLogIn);
-            localStorage.setItem('currentUser', IDString);
+            // var IDString = JSON.stringify(currentLogIn);
+            //udkommenteret fordi currentLogin stadig ikke findes
+            //localStorage.setItem('currentUser', IDString);
+            //udkommenteret
 
             // laver en currentUser2 for ikke at ødelægge noget i den oprindelige (kan blive slettet, hvis det hele kører)
             // JSON.stringify converts a JavaScript object or value to JSON string (JavaScript Object Notation)
             localStorage.setItem('currentUser2', JSON.stringify(existingUser[i]));
 
+            location.href = "loggedIn.html";
+
             alert("you are logged in");
             return true;
-
-        } else {
-            alert("ERROR: Wrong details entered");
         }
     }
-            }
+    alert("wrong!")
+}
 
     //if input doesnt match existing user, show to user also show amount of attempts left
     /*attempt--;
