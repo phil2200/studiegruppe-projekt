f87017d157a35e5c3363047367f2c29b612e259a
ahellow <ahellow@users.noreply.github.com>
2019-11-29 10:53:27 +0100

    opdateringer og error fiksninger

--- SignUp_SignIn/Showpassword.js
+++ SignUp_SignIn/Showpassword.js
@@ -3,9 +3,9 @@
 
 function showPassword() {
     var x = document.getElementById("password");
-          if ( x.type == "password") {
-               x.type = "text";
-                     } else {
-                 x.type = "password";
-          }
-        }
+    if ( x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
--- SignUp_SignIn/SignIn.js
+++ SignUp_SignIn/SignIn.js
@@ -4,43 +4,27 @@ var existingUser = JSON.parse(localStorage.getItem('User'));
 
 // when clicking submit, retrieve input from username and password box.
 
-    function login() {
-      var username = document.getElementById("username").value;
-      var password = document.getElementById("password").value;
-
-    //check if entered details match stored details. redirect to home page
-         for (let i = 0; i < existingUser.length; i++) {
-              if (username === existingUser[i].username && password === existingUser[i].password) {
-
-            // EMDL: laver en currentUser, som skal vise brugeroplysninger på den bruger, der logger ind
-            // EMDL: JSON.stringify converts a JavaScript object or value to JSON string (JavaScript Object Notation)
-                 localStorage.setItem('currentUser', JSON.stringify(existingUser[i]));
-
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
+    //check if entered details match stored details. redirect to home page
+    for (let i = 0; i < existingUser.length; i++) {
+        if (username === existingUser[i].username && password === existingUser[i].password) {
 
-    function login() {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        //check if entered details match stored details. redirect to home page
-        for (let i = 0; i < existingUser.length; i++) {
-            if (username === existingUser[i].username && password === existingUser[i].password) {
+            // EMDL: laver en currentUser, som skal vise brugeroplysninger på den bruger, der logger ind
+            // EMDL: JSON.stringify converts a JavaScript object or value to JSON string (JavaScript Object Notation)
+            localStorage.setItem('currentUser', JSON.stringify(existingUser[i]));
+            location.href = "loggedIn.html";
 
-                // EMDL: laver en currentUser, som skal vise brugeroplysninger på den bruger, der logger ind
-                // EMDL: JSON.stringify converts a JavaScript object or value to JSON string (JavaScript Object Notation)
-                localStorage.setItem('currentUser', JSON.stringify(existingUser[i]));
-                location.href = "loggedIn.html";
-
-                // EMDL: alert "you are logged in", hvis oplysninger er korrekte og allers aleret "Wrong username or password"
-                alert("You are logged in");
-                return true;
-            }
+            // EMDL: alert "you are logged in", hvis oplysninger er korrekte og allers aleret "Wrong username or password"
+            alert("You are logged in");
+            return true;
         }
-        alert("Wrong username or password")
     }
+    alert("Wrong username or password")
 }
+
 
 
 /*
@@ -72,14 +56,14 @@ function login() {
 
  */
 
-    //if input doesnt match existing user, show to user also show amount of attempts left
- /*   attempt--;
-    alert("Username or Password invalid. You have" + attempt + "attempts left");
-    if (attempt == 0) {
-        document.getElementById("username").disabled = true;
-        document.getElementById("password").disabled = true;
-        document.getElementById("submit").disabled = false;
+//if input doesnt match existing user, show to user also show amount of attempts left
+/*   attempt--;
+   alert("Username or Password invalid. You have" + attempt + "attempts left");
+   if (attempt == 0) {
+       document.getElementById("username").disabled = true;
+       document.getElementById("password").disabled = true;
+       document.getElementById("submit").disabled = false;
 
-        alert("Access denied. Are you sure you registered? Go to our sign up page and try again :) ");
-        return false;
-    } */
+       alert("Access denied. Are you sure you registered? Go to our sign up page and try again :) ");
+       return false;
+   } */
