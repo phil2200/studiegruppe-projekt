6833685d11da3b145fc0879767b6d98b34aaf89f
Emil Juel Jakobsen <Emja19ai@student.cbs.dk>
2019-11-29 12:36:44 +0100

    Productsdone

--- Products/products.js
+++ Products/products.js
@@ -19,9 +19,9 @@ for(var i=0; i<products.length; i++){
    /* tager Child af productsContainer */
    productsContainer.appendChild(product);
 }
-function interProduct(productId) {
+
+function interProduct (productId) {
    window.location.replace('interProduct.html?productId='+productId);
-   window.history.go(-1)
 }
 
 
--- Products/singleProduct.js
+++ Products/singleProduct.js
@@ -1,68 +1,85 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-var productId = url.searchParams.get("productId");
+//URL SearchParams. Dette er den måde hvorpå når der trykkes på et billede i shopsiden bliver man sendt til det pågældende produktsside
+var urlParams = new URLSearchParams(location.search);
+var productId = urlParams.get("productId")
+
+var product = findProduct(productId);
+
 
 /* variabel som indhenter productsContainer i HTMLfilen */
 var productContainer = document.getElementById('product');
 
-for(var i=0; i<products.length; i++) {
-    if(products[i]._productId == productId) {
+var container = document.createElement('container');
 
-        var container = document.createElement('container');
+/* Her bliver de forskellige properties fra klassen udvalgt og kan displayes på siden */
+container.className = "container";
 
-        /* Her bliver de forskellige properties fra klassen udvalgt og kan displayes på siden */
-        container.className = "container";
-
-        /* displayer image */
-        container.innerHTML = '<img class="product_img" src=' + products[i]._productImage + ' onClick="interProduct('+products[i]._productId+')">';
-        /* displayer produktnavnet */
-        container.innerHTML += '<div class="productName">' + products[i]._productName + '</div>';
-        /* displayer prisen */
-        container.innerHTML += '<div class="productPrice"> Price: ' + products[i]._productPrice + ' kr </div>';
-        /* displayer farven */
-        container.innerHTML += '<div class="productColor"> Color: ' + products[i]._colors + '</div>';
-        /* displayer størrelsen */
-        container.innerHTML += '<div class="productSize">';
-
-        // Valg af størrelse, her gøres der brug af en selector, samt bliver de forskellige sizes loopet
-            var select = '<select class="productSizeSelect"> Pick a size';
-
-                //Når du skal have den valgte værdi skriver du
-                //var selectElement = document.getElementByClass('productSizeSelect');
-                //var selectedSize = selectElement.options[selectElement.selectedIndex].value;
-
-                var sizes = products[i]._productSize;
-                for (var i=0; i<sizes.length; i++){
-                    select += '<option value="'+i+'">'+ sizes[i] + '</option>';
-                }
-
-            select += '</select>';
-        container.innerHTML += select;
-        container.innerHTML += '</div>';
-
-        /* Add to cart button, som gør at detaljerne fra det valgte produkt kan videreføres til shoppingcart */
-
-            var addToCart = '<button type="button" class ="addToCartButton">Add item to cart';
-
-            function productDetailTransfer(){
-                var productPick = productId[i]
+/* displayer image */
+container.innerHTML = '<img class="product_img" src=' + product._productImage + ' onClick ="interProduct"(' + product._productId + ')">';
+/* displayer produktnavnet */
+container.innerHTML += '<div class="productName">' + product._productName + '</div>';
+/* displayer prisen */
+container.innerHTML += '<div class="productPrice"> Price: ' + product._productPrice + ' kr </div>';
+/* displayer farven */
+container.innerHTML += '<div class="productColor"> Color: ' + product._colors + '</div>';
+/* displayer størrelsen */
+container.innerHTML += '<div class="productSize">';
 
 
+// Valg af størrelse, her gøres der brug af en selector, samt bliver de forskellige sizes loopet
+var select = '<select class="productSizeSelect">';
 
-
-
-            }
-
-        container.innerHTML +=addToCart;
-        container.innerHTML += '</div>'
-
-        /*displayer Status*/
-        container.innerHTML += '<div class="stockStatus"> Status: ' + products[i]._stockStatus + '</div>';
-        /*displayer beskrivelsen*/
-        container.innerHTML += '<div class="productDesc"> Description: ' + products[i]._productDesc + '</div>';
-
-        /* tager Child af productsContainer */
-        productContainer.appendChild(container);
-    }
+var sizes = product._productSize;
+for (var i = 0; i < sizes.length; i++) {
+    select += '<option value="' + i + '">' + sizes[i] + '</option>';
 }
 
+select += '</select>';
+container.innerHTML += select;
+container.innerHTML += '</div>';
+
+/* Add to cart button, som gør at detaljerne fra det valgte produkt kan videreføres til shoppingcart*/
+var addToCart = '<button type="button" class ="addToCartBtn" onclick="addToShoppingCart()">Add item to cart';
+container.innerHTML += addToCart;
+
+
+/*displayer Status*/
+container.innerHTML += '<div class="stockStatus"> Status: ' + product._stockStatus + '</div>';
+
+/*displayer beskrivelsen*/
+container.innerHTML += '<div class="productDesc"> Description: ' + product._productDesc + '</div>';
+
+/* tager Child af productsContainer */
+productContainer.appendChild(container);
+
+
+function addToShoppingCart() {
+    var productToCart = product;
+
+    //Hent den størrelse der er valgt
+    var selectElement = document.getElementsByClassName('productSizeSelect')[0];
+    var selectedSize = selectElement.options[selectElement.selectedIndex].innerText;
+
+    //Sæt størrelsen til den valgte størrelse
+    productToCart._productSize = selectedSize;
+
+    //Hent vores nuværende cart fra localstorage
+    var cart = localStorage.getItem('cart');
+    if (cart == null) {
+        cart = [];
+    } else {
+        cart = JSON.parse(cart);
+    }
+
+    cart.push(productToCart);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+function findProduct(productId) {
+    for (var i = 0; i < products.length; i++) {
+        if (products[i]._productId == productId) {
+            return products[i];
+        }
+    }
+}
