c7d01c73e8cc126f84d93e40a0a13ebb8994d3a6
ahellow <ahellow@users.noreply.github.com>
2019-12-08 16:11:32 +0100

    fiksing

--- SignUp_SignIn/signInCaller.js
+++
@@ -1,8 +0,0 @@
-
-        // function der kører de funktioner vi har brug for ved sign in/log in.
-        // ikke i brug pga. predefined users.
-        function signInCaller() {
-            checkLogin();
-
-
-        }
